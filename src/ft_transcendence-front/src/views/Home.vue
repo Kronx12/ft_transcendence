47a2909d01373e5966a51345665eff658ee7fcdc
Guillaume Baud <40308463+Kronx12@users.noreply.github.com>
2021-06-19 00:15:36 +0200

    Patch error

--- src/ft_transcendence-front/src/views/Home.vue
+++ src/ft_transcendence-front/src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
-    <div>
-      <h1>Bonjour</h1>
-    <router-link v-if="this.$store.state.user.id != -1" to="/queue">Join Game</router-link>
-    <div id="main">
+  <div>
+    <h1>Bonjour</h1>
+    <router-link v-if="$store.state.user.id != -1" to="/queue">Join Game</router-link>
+    <!-- <div id="main">
       <div id="leaderboard">
         <h3>Leaderbord du cul</h3>
       </div>
@@ -12,14 +12,14 @@
       <div id="chat">
         <h3>Chat</h3>
       </div>
-    </div>
+    </div> -->
   </div>
 </template>
 
 <script>
 
 export default {
-  mounted: function() {
+  mounted() {
     if (this.$store.state.user.id == -1)
       this.$router.push('/login')
   }
